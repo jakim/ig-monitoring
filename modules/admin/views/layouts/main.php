80d5f91d064d6e0b81fecb0cc1b2087457e5db1f
Paweł Jakimowski <pawel@jakimowski.info>
2018-02-28 06:10:54 +0100

    fix: mini logo

--- modules/admin/views/layouts/main.php
+++ modules/admin/views/layouts/main.php
@@ -30,7 +30,7 @@ $user = Yii::$app->user->identity;
 
     <header class="main-header">
 
-        <?= Html::a('<span class="logo-mini">APP</span><span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
+        <?= Html::a('<span class="logo-mini">IGM</span><span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
 
         <nav class="navbar navbar-static-top" role="navigation">
 
