65aacbd5a8ec091781f534a522ce453fe1603500
Paweł Jakimowski <pawel@jakimowski.info>
2018-05-05 16:02:14 +0200

    chg: proxy alert, closes #40

--- modules/admin/widgets/ProxyAlert.php
+++ modules/admin/widgets/ProxyAlert.php
@@ -10,6 +10,7 @@ namespace app\modules\admin\widgets;
 
 use app\models\Proxy;
 use yii\base\Widget;
+use yii\helpers\Html;
 
 class ProxyAlert extends Widget
 {
@@ -19,7 +20,7 @@ class ProxyAlert extends Widget
         if (!Proxy::find()->active()->exists()) {
             echo "<section class=\"content-header\">";
             echo "<div class=\"alert alert-error\">";
-            echo "Add at least one proxy.";
+            echo Html::a('Add at least one proxy <span class="fa fa-angle-double-right"></span>', ['proxy/create']);
             echo "</div>";
             echo "</section>";
         }
