54c98ebd1c55a2cef9b09d5c161c0d71fe43d2ba
Paweł Jakimowski <pawel@jakimowski.info>
2018-05-07 09:19:45 +0200

    chg: generic error refactoring, refers #34 @ api

--- modules/api/v1/controllers/DefaultController.php
+++ modules/api/v1/controllers/DefaultController.php
@@ -21,32 +21,66 @@ class DefaultController extends Controller
      */
     public function actionError()
     {
+        $exception = $this->getException();
+
+        $name = $this->getName($exception);
+
+        $message = $this->getMessage($exception);
+
+        return $this->prepareResponse($name, $message, $exception);
+    }
+
+    protected function getException()
+    {
         if (($exception = \Yii::$app->getErrorHandler()->exception) === null) {
             // action has been invoked not from error handler, but by direct route, so we display '404 Not Found'
             $exception = new NotFoundHttpException('Endpoint not found.');
         }
 
+        return $exception;
+    }
+
+    protected function getCode(Exception $exception): int
+    {
         if ($exception instanceof HttpException) {
             $code = $exception->statusCode;
         } else {
             $code = $exception->getCode();
         }
 
+        return $code;
+    }
+
+    protected function getName(Exception $exception): string
+    {
         if ($exception instanceof Exception) {
             $name = $exception->getName();
         } else {
             $name = 'Error';
         }
+
+        $code = $this->getCode($exception);
+
         if ($code) {
             $name .= " (#$code)";
         }
 
+        return $name;
+    }
+
+    protected function getMessage(Exception $exception): string
+    {
         if ($exception instanceof UserException) {
             $message = $exception->getMessage();
         } else {
             $message = 'An internal server error occurred.';
         }
 
+        return $message;
+    }
+
+    protected function prepareResponse($name, $message, $exception): array
+    {
         \Yii::$app->response->format = Response::FORMAT_JSON;
 
         return [
