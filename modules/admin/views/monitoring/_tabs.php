ecfb756689b754551851a68217f6d791cda6e927
Paweł Jakimowski <pawel@jakimowski.info>
2018-05-22 17:35:07 +0200

    Closes #51 clear monitoring tabs

--- modules/admin/views/monitoring/_tabs.php
+++ modules/admin/views/monitoring/_tabs.php
@@ -2,8 +2,8 @@
     'items' => [
         ['label' => 'Accounts', 'url' => ['/admin/monitoring/accounts']],
         ['label' => 'Tags', 'url' => ['/admin/monitoring/tags']],
-        ['label' => 'Locations (TODO)'],
-        ['label' => 'Groups (TODO)'],
+//        ['label' => 'Locations (TODO)'],
+//        ['label' => 'Groups (TODO)'],
     ],
     'options' => ['class' => 'nav nav-tabs'], // set this to nav-tabs or nav-pills to get tab-styled navigation
 ]); ?>
