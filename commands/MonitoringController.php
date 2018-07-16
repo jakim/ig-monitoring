0e2339edb4417524503c2485fe134a2599694588
Paweł Jakimowski <pawel@jakimowski.info>
2018-07-16 14:00:09 +0200

    chg: removed unused commands @ monitoring

--- commands/MonitoringController.php
+++ commands/MonitoringController.php
@@ -25,18 +25,6 @@ class MonitoringController extends Controller
         $this->actionTags();
     }
 
-    /**
-     * NOTE: Moved to admin panel.
-     *
-     * @deprecated
-     */
-    public function actionAccount()
-    {
-        $this->stdout("moved to admin panel\n");
-
-        return ExitCode::OK;
-    }
-
     public function actionAccounts()
     {
         echo Table::widget([
@@ -57,32 +45,6 @@ class MonitoringController extends Controller
         ]);
     }
 
-    public function actionTag($name, $proxy_id = null)
-    {
-        $tag = Tag::findOne(['name' => $name]);
-        if ($tag === null) {
-            $tag = new Tag(['name' => $name]);
-            $mainTag = Tag::findOne(['slug' => Inflector::slug($name)]);
-            if ($mainTag) {
-                $tag->main_tag_id = $mainTag->id;
-            }
-        }
-
-        if (!$this->checkProxy($tag, $proxy_id)) {
-            return ExitCode::UNSPECIFIED_ERROR;
-        }
-
-        $tag->monitoring = 1;
-        if (!$tag->save()) {
-            echo Console::errorSummary($tag);
-
-            return ExitCode::DATAERR;
-        }
-        $this->stdout("OK!\n");
-
-        return ExitCode::OK;
-    }
-
     public function actionTags()
     {
         echo Table::widget([
@@ -101,32 +63,5 @@ class MonitoringController extends Controller
                 ->asArray()
                 ->all(),
         ]);
-    }
-
-    /**
-     * @param Account|Tag $model
-     * @param $proxy_id
-     * @return bool
-     */
-    private function checkProxy($model, $proxy_id)
-    {
-        $proxy = Proxy::findOne(['id' => $proxy_id]);
-
-        if ($proxy_id && $proxy === null) {
-            $this->stdout("ERR: Proxy '$proxy_id' not found.\n", Console::FG_RED);
-
-            return false;
-
-        }
-
-        if (!$proxy && !$model->getProxy()) {
-            $this->stdout("ERR: There MUST be at least one valid proxy.\n", Console::FG_RED);
-
-            return false;
-        }
-
-        $model->proxy_id = $proxy ? $proxy->id : null;
-
-        return true;
     }
 }
