55127354333f802167fe411e46b44ffdbb2be292
Paweł Jakimowski <pawel@jakimowski.info>
2018-08-23 15:53:42 +0200

    add: tooltip with invalidation description @ monitoring/accounts

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -7,7 +7,10 @@
 
 namespace app\assets;
 
+use yii\bootstrap\BootstrapAsset;
+use yii\bootstrap\BootstrapPluginAsset;
 use yii\web\AssetBundle;
+use yii\web\YiiAsset;
 
 /**
  * Main application asset bundle.
@@ -25,7 +28,8 @@ class AppAsset extends AssetBundle
     public $js = [
     ];
     public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        YiiAsset::class,
+        BootstrapAsset::class,
+        BootstrapPluginAsset::class,
     ];
 }
--- modules/admin/views/monitoring/accounts.php
+++ modules/admin/views/monitoring/accounts.php
@@ -20,86 +20,90 @@ $this->params['breadcrumbs'][] = 'Accounts';
 /** @var \app\components\Formatter $formatter */
 $formatter = Yii::$app->formatter;
 ?>
-<div class="account-index nav-tabs-custom">
+    <div class="account-index nav-tabs-custom">
 
-    <?= $this->render('_tabs') ?>
+        <?= $this->render('_tabs') ?>
 
-    <div class="tab-content table-responsive">
-        <?= GridView::widget([
-            'dataProvider' => $dataProvider,
-            'filterModel' => $searchModel,
-            'columns' => [
-                ['class' => \yii\grid\SerialColumn::class],
-                [
-                    'attribute' => 'username',
-                    'content' => function (\app\models\Account $model) {
-                        $html = [];
-                        $html[] = Html::a($model->displayName, ['account/dashboard', 'id' => $model->id]);
-                        $html[] = Html::a('<span class="fa fa-external-link text-sm"></span>', Url::account($model->username), ['target' => '_blank']);
+        <div class="tab-content table-responsive">
+            <?= GridView::widget([
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'columns' => [
+                    ['class' => \yii\grid\SerialColumn::class],
+                    [
+                        'attribute' => 'username',
+                        'content' => function (\app\models\Account $model) {
+                            $html = [];
+                            $html[] = Html::a($model->displayName, ['account/dashboard', 'id' => $model->id]);
+                            $html[] = Html::a('<span class="fa fa-external-link text-sm"></span>', Url::account($model->username), ['target' => '_blank']);
 
-                        if (!$model->is_valid) {
-                            $html[] = sprintf(
-                                '<span class="fa fa-exclamation-triangle text-danger pull-right" title="%s"></span>',
-                                \app\dictionaries\AccountInvalidationType::getLabel($model->invalidation_type_id, 'Unknown reason')
-                            );
-                        }
-                        if ($model->accounts_monitoring_level) {
-                            $html[] = sprintf('<span class="fa fa-magic text-muted pull-right" title="monitoring level: %s"></span>', $model->accounts_monitoring_level);
-                        }
-                        if ($model->disabled) {
-                            $html[] = '<span class="fa fa-exclamation-triangle text-danger pull-right" title="Not found."></span>';
-                        }
+                            if (!$model->is_valid) {
+                                $html[] = sprintf(
+                                    '<span class="fa fa-exclamation-triangle text-danger pull-right" data-toggle="tooltip" data-placement="top"  title="%s, attempts: %s"></span>',
+                                    \app\dictionaries\AccountInvalidationType::getLabel($model->invalidation_type_id, 'Unknown reason'),
+                                    $model->invalidation_count
+                                );
+                            }
+                            if ($model->accounts_monitoring_level) {
+                                $html[] = sprintf('<span class="fa fa-magic text-muted pull-right" title="monitoring level: %s"></span>', $model->accounts_monitoring_level);
+                            }
+                            if ($model->disabled) {
+                                $html[] = '<span class="fa fa-exclamation-triangle text-danger pull-right" title="Not found."></span>';
+                            }
 
-                        return implode(" \n", $html);
-                    },
-                ],
-                [
-                    'class' => StatsColumn::class,
-                    'attribute' => 'as_followed_by',
-                    'statsAttribute' => 'followed_by',
-                    'dailyDiff' => $dailyDiff,
-                    'monthlyDiff' => $monthlyDiff,
-                ],
-                [
-                    'class' => StatsColumn::class,
-                    'attribute' => 'as_follows',
-                    'statsAttribute' => 'follows',
-                    'dailyDiff' => $dailyDiff,
-                    'monthlyDiff' => $monthlyDiff,
-                ],
-                [
-                    'class' => StatsColumn::class,
-                    'attribute' => 'as_media',
-                    'statsAttribute' => 'media',
-                    'dailyDiff' => $dailyDiff,
-                    'monthlyDiff' => $monthlyDiff,
-                ],
-                [
-                    'class' => StatsColumn::class,
-                    'attribute' => 'as_er',
-                    'statsAttribute' => 'er',
-                    'dailyDiff' => $dailyDiff,
-                    'monthlyDiff' => $monthlyDiff,
-                    'numberFormat' => [
-                        'percent',
-                        2,
-                        ['sign' => false],
+                            return implode(" \n", $html);
+                        },
+                    ],
+                    [
+                        'class' => StatsColumn::class,
+                        'attribute' => 'as_followed_by',
+                        'statsAttribute' => 'followed_by',
+                        'dailyDiff' => $dailyDiff,
+                        'monthlyDiff' => $monthlyDiff,
+                    ],
+                    [
+                        'class' => StatsColumn::class,
+                        'attribute' => 'as_follows',
+                        'statsAttribute' => 'follows',
+                        'dailyDiff' => $dailyDiff,
+                        'monthlyDiff' => $monthlyDiff,
+                    ],
+                    [
+                        'class' => StatsColumn::class,
+                        'attribute' => 'as_media',
+                        'statsAttribute' => 'media',
+                        'dailyDiff' => $dailyDiff,
+                        'monthlyDiff' => $monthlyDiff,
+                    ],
+                    [
+                        'class' => StatsColumn::class,
+                        'attribute' => 'as_er',
+                        'statsAttribute' => 'er',
+                        'dailyDiff' => $dailyDiff,
+                        'monthlyDiff' => $monthlyDiff,
+                        'numberFormat' => [
+                            'percent',
+                            2,
+                            ['sign' => false],
+                        ],
+                    ],
+                    's_tags',
+                    [
+                        'attribute' => 'as_created_at',
+                        'label' => 'Updated At',
+                        'format' => 'date',
+                    ],
+                    [
+                        'attribute' => 'created_at',
+                        'format' => 'date',
                     ],
                 ],
-                's_tags',
-                [
-                    'attribute' => 'as_created_at',
-                    'label' => 'Updated At',
-                    'format' => 'date',
-                ],
-                [
-                    'attribute' => 'created_at',
-                    'format' => 'date',
-                ],
-            ],
-        ]); ?>
+            ]); ?>
 
-        <?= \app\modules\admin\widgets\CreateMonitoringModal::widget() ?>
-        <?= \app\modules\admin\widgets\favorites\AddToModal::widget() ?>
+            <?= \app\modules\admin\widgets\CreateMonitoringModal::widget() ?>
+            <?= \app\modules\admin\widgets\favorites\AddToModal::widget() ?>
+        </div>
     </div>
-</div>
+
+<?php
+$this->registerJs('jQuery(\'[data-toggle="tooltip"]\').tooltip()');
