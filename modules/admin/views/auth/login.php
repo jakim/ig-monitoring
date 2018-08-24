d155b667ca48e786fa232a19f23f5d17c2304383
Paweł Jakimowski <pawel@jakimowski.info>
2018-08-24 17:46:50 +0200

    chg: login page

--- modules/admin/views/auth/login.php
+++ modules/admin/views/auth/login.php
@@ -4,11 +4,21 @@
  * User: jakim <pawel@jakimowski.info>
  * Date: 26.01.2018
  */
+
+use yii\authclient\widgets\AuthChoice;
+use yii\helpers\Html;
+
 ?>
 
-<p class="login-box-msg">Sign in to start your session</p>
+    <p class="login-box-msg">Sign in to start your session</p>
 
-<?= yii\authclient\widgets\AuthChoice::widget([
-    'baseAuthUrl' => ['/admin/auth/auth'],
+<?php $authAuthChoice = AuthChoice::begin([
     'popupMode' => false,
+    'autoRender' => false,
 ]) ?>
+<?php foreach ($authAuthChoice->getClients() as $client): ?>
+    <div class="form-group">
+        <?= Html::a('Sign in with ' . $client->getTitle(), ['/admin/auth/auth', 'authclient' => $client->getName(),], ['class' => "btn btn-success btn-block $client->name "]) ?>
+    </div>
+<?php endforeach; ?>
+<?php AuthChoice::end() ?>
