b2168e1508d9a20c57268101c3a21ca74c0d1751
Paweł Jakimowski <pawel@jakimowski.info>
2019-05-13 09:23:41 +0200

    fix: environment settings

--- config/env.php
+++ config/env.php
@@ -6,5 +6,5 @@
  */
 
 // comment out the following two lines when deployed to production
-//defined('YII_DEBUG') or define('YII_DEBUG', true);
-//defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
