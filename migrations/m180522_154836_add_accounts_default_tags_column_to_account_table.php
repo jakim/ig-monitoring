bc688fc215e1d62117c1dd11268d09b66b51c2c0
Ali Error <alipadida@live.com>
2019-08-10 15:46:14 +0430

    Fixed Incorrect string value for unicode languages
    The fix adds support for unicode languages in accounts_default_tags

--- migrations/m180522_154836_add_accounts_default_tags_column_to_account_table.php
+++ migrations/m180522_154836_add_accounts_default_tags_column_to_account_table.php
@@ -12,7 +12,7 @@ class m180522_154836_add_accounts_default_tags_column_to_account_table extends M
      */
     public function safeUp()
     {
-        $this->addColumn('account', 'accounts_default_tags', $this->string());
+        $this->addColumn('account', 'accounts_default_tags', $this->string()->append('CHARACTER SET utf8 COLLATE utf8_unicode_ci'));
     }
 
     /**
