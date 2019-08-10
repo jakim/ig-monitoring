3478c18a4ae281f3eb895e1847e62e1907a71185
Ali Error <alipadida@live.com>
2019-08-10 15:41:58 +0430

    Fixed Incorrect string value for unicode languages
    The fix adds support for unicode languages in label

--- migrations/m180201_155243_create_favorite_table.php
+++ migrations/m180201_155243_create_favorite_table.php
@@ -14,7 +14,7 @@ class m180201_155243_create_favorite_table extends Migration
     {
         $this->createTable('favorite', [
             'id' => $this->primaryKey(),
-            'label' => $this->string()->notNull(),
+            'label' => $this->string()->notNull()->append('CHARACTER SET utf8 COLLATE utf8_unicode_ci'),
             'url' => $this->string()->notNull(),
             'created_at' => $this->dateTime(),
         ]);
